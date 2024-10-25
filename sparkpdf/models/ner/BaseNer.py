94f57623be9d0eed4739e1f7dcc37d6f4183b189
Mykola Melnyk <kolia1985@gmail.com>
2024-10-25 13:58:15 +0300

    Added Ner

---
+++ sparkpdf/models/ner/BaseNer.py
@@ -0,0 +1,66 @@
+
+
+import json
+from pyspark.ml.param import Param, Params, TypeConverters
+from pyspark.sql.types import *
+from pyspark.ml import Transformer
+from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.sql.functions import pandas_udf, lit, udf
+
+from sparkpdf.params import *
+from sparkpdf.schemas.NerOutput import NerOutput
+
+
+class BaseNer(Transformer, HasInputCol, HasOutputCol, HasKeepInputData, HasWhiteList, HasDevice, HasModel, HasPathCol,
+          DefaultParamsReadable, DefaultParamsWritable, HasNumPartitions, HasScoreThreshold, HasBatchSize, HasPageCol):
+
+    def outputSchema(self):
+        return StructType([StructField("path", StringType(), True),
+                               StructField("entities",
+                                           ArrayType(StructType([StructField("entity_group", StringType(), False),
+                                                                 StructField("score", DoubleType(), False),
+                                                                 StructField("word", StringType(), False),
+                                                                 StructField("start", IntegerType(), False),
+                                                                 StructField("end", IntegerType(), False),
+                                                                 StructField("boxes", ArrayType(StructType(
+                                                                     [StructField("text", StringType(), False)
+                                                                         , StructField("score", DoubleType(), False),
+                                                                      StructField("x", IntegerType(), False),
+                                                                      StructField("y", IntegerType(), False),
+                                                                      StructField("width", IntegerType(), False),
+                                                                      StructField("height", IntegerType(), False)]),
+                                                                                                True),
+                                                                             False)]),
+                                                     True),
+                                           True),
+                               StructField("exception", StringType(), True)])
+
+    def _transform(self, dataset):
+
+        params = json.dumps({k.name: v for k, v in self.extractParamMap().items()})
+        out_col = self.getOutputCol()
+        if self.getInputCol() not in dataset.columns:
+            input_col = self.getInputCol()
+            raise ValueError(f"""Missing input column in {self.uid}: Column '{input_col}' is not present.""")
+        in_col = dataset[self.getInputCol()]
+
+        if not hasattr(dataset, "sparkSession"):
+            result = dataset.withColumn(out_col,
+                                        udf(self.transform_udf, NerOutput.get_schema())
+                                        (in_col))
+        else:
+            if self.getNumPartitions() > 0:
+                if self.getPageCol() in dataset.columns:
+                    dataset = dataset.repartition( self.getPageCol())
+                else:
+                    dataset = dataset.repartition(self.getPathCol())
+                dataset = dataset.coalesce(self.getNumPartitions())
+            result = dataset.withColumn(out_col,
+                                        pandas_udf(self.transform_udf_pandas, self.outputSchema())
+                                        (in_col, lit(params)))
+
+        if not self.getKeepInputData():
+            result = result.drop(in_col)
+        return result
+
+
---
+++ tests/models/ner/test_ner.py
@@ -0,0 +1,22 @@
+from sparkpdf.enums import Device
+from sparkpdf.models.ner.Ner import Ner
+from sparkpdf.models.recognizers.TesseractOcr import TesseractOcr
+#from sparkpdf.image.ImageDrawBoxes import ImageDrawBoxes
+
+import pyspark.sql.functions as f
+import pytest
+import logging
+from pyspark.ml.pipeline import PipelineModel
+
+def test_ner(image_df):
+    ocr = TesseractOcr()
+    ner = Ner(model="obi/deid_bert_i2b2", numPartitions=0, device=Device.CUDA.value)
+    result_df = ner.transform(ocr.transform(image_df))
+    result = result_df.select("ner").cache()
+    data = result.collect()
+    assert (len(data) == 1)
+    # present ner field
+    assert (hasattr(data[0], "ner"))
+    ner_tags = result.select(f.explode("ner.entities").alias("ner")).select("ner.*")
+    ner_tags.show(40)
+    assert (ner_tags.count() == 73)
