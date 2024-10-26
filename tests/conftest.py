fdf4ec8aedb6e8139a1fff2afc0ce54386c643bb
Mykola Melnyk <kolia1985@gmail.com>
2024-10-26 15:10:35 +0300

    Updated fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,24 +4,26 @@ from sparkpdf.image.DataToImage import DataToImage
 from sparkpdf.enums import ImageType
 
 @pytest.fixture
-def raw_image_df(spark_session):
-    return spark_session.read.format("binaryFile").load("../sparkpdf/resources/images/InvoiceforMedicalRecords_10_722.png")
+def raw_image_df(spark_session, resource_path_root):
+    return spark_session.read.format("binaryFile").load(
+        (resource_path_root / "images/InvoiceforMedicalRecords_10_722.png").absolute().as_posix())
 
 
 @pytest.fixture
-def binary_pdf_df(spark_session):
+def binary_pdf_df(spark_session, resource_path_root):
     df = spark_session.read.format("binaryFile").load(
-        "../sparkpdf/resources/pdfs/unipdf-medical-bill.pdf")
+        (resource_path_root / "pdfs/unipdf-medical-bill.pdf").absolute().as_posix())
     return df
 
 @pytest.fixture
-def pdf_df(spark_session):
+def pdf_df(spark_session, resource_path_root):
     df = spark_session.read.format("binaryFile").load(
-        "../sparkpdf/resources/pdfs/unipdf-medical-bill.pdf")
+        (resource_path_root / "pdfs/unipdf-medical-bill.pdf").absolute().as_posix())
     return df
 
 @pytest.fixture
-def image_df(spark_session):
-    df = spark_session.read.format("binaryFile").load("../sparkpdf/resources/images/InvoiceforMedicalRecords_10_722.png")
+def image_df(spark_session, resource_path_root):
+    df = spark_session.read.format("binaryFile").load(
+        (resource_path_root / "images/InvoiceforMedicalRecords_10_722.png").absolute().as_posix())
     bin_to_image = DataToImage().setImageType(ImageType.WEBP.value)
     return bin_to_image.transform(df)
