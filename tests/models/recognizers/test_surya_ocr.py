2600ca96adde4f95c26f784e0b273f6e56a3ee45
Mykola Melnyk <kolia1985@gmail.com>
2024-11-16 11:19:07 +0100

    Added Doctrocr

--- tests/models/recognizers/test_surya_ocr.py
+++ tests/models/recognizers/test_surya_ocr.py
@@ -5,13 +5,13 @@ from sparkpdf.enums import Device
 
 def test_surya_ocr_class(image_line):
     # Test SuryatOcr
-    result1 = SuryaOcr(keepFormatting=False, partitionMap=False, device=Device.CUDA).transform_udf(image_line)
+    result1 = SuryaOcr(keepFormatting=False, partitionMap=False, device=Device.CPU).transform_udf(image_line)
     assert result1.exception == "", "Expected no exception for keepFormatting=False"
     assert "24/11/16 08:12:24 WARN Utils: Service 'SparkUI' could not bind on port 4043. Attempting port 4044." in result1.text
 
 
 def test_surya_ocr_pandas_udf(image_line_df):
-    ocr = SuryaOcr(partitionMap=True, device=Device.CUDA, keepFormatting=True)
+    ocr = SuryaOcr(partitionMap=True, device=Device.CPU, keepFormatting=True)
 
     # Transform the image dataframe through the OCR stage
     result = ocr.transform(image_line_df).collect()
