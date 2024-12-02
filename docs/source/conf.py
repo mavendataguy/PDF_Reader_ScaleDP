756e12029f8cc06de33bc7266e7dcdaf38fd3718
Mykola Melnyk <kolia1985@gmail.com>
2024-12-02 15:25:53 +0300

    Updated docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,9 @@ exclude_patterns = []
 html_theme = 'sphinx_book_theme'
 html_static_path = ['_static']
 
+html_favicon = '_static/favicon.ico'
+html_logo = '_static/ScaleDP.png'
+
 
 
 # -- Options for HTML output -------------------------------------------------
